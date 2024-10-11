f3ceb89d6e9dde58218063594c1598f6e7e2d5b3
Matti Airas <matti.airas@hatlabs.fi>
2024-10-11 23:15:04 +0300

    Allow setting the resolution and conversion delay for OneWire temperature sensors

--- src/sensesp_onewire/onewire_temperature.cpp
+++ src/sensesp_onewire/onewire_temperature.cpp
@@ -29,8 +29,12 @@ bool string_to_owda(OWDevAddr* addr, const char* str) {
   return num_items == 8;
 }
 
-DallasTemperatureSensors::DallasTemperatureSensors(int pin, String config_path, DSTherm::Resolution res)
-    : Sensor(config_path) {
+DallasTemperatureSensors::DallasTemperatureSensors(
+    int pin, String config_path, DSTherm::Resolution resolution,
+    uint32_t conversion_delay)
+    : Sensor(config_path),
+      resolution_{resolution},
+      conversion_delay_{conversion_delay} {
   onewire_ = new OneWireNg_CurrentPlatform(pin,
                                            false  // disable internal pull-up
   );
@@ -97,7 +101,10 @@ bool DallasTemperatureSensors::get_next_address(OWDevAddr* addr) {
 
 OneWireTemperature::OneWireTemperature(DallasTemperatureSensors* dts,
                                        uint read_delay, String config_path)
-    : sensesp::FloatSensor(config_path), dts_{dts}, read_delay_{read_delay} {
+    : sensesp::FloatSensor(config_path),
+      dts_{dts},
+      read_delay_{read_delay},
+      conversion_delay_{dts->get_conversion_delay()} {
   load();
   if (address_ == null_ow_addr) {
     // previously unconfigured sensor
